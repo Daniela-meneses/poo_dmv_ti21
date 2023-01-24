a84818a69e2a87573abe665276624ef42f00a7dc
dany <122809726+Daniela-meneses@users.noreply.github.com>
2023-01-24 19:40:27 +0000

    se creó el programa1.py

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 |No.|Archivo|Descripcion|
 |--|--|--|
 |1.|Programa0.py|Hola mundo en python|
+|2.|Programa1.py|programa para ver programas multilinea,comentarios de una linea,concatenar cadenas|
---
+++ semana2/programa1.py
@@ -0,0 +1,9 @@
+"""programa1
+Nombre: Daniela M,V
+Fecha: 23/01/23
+Descripcion: programa para ver programas multilinea,comentarios de una linea,concatenar cadenas 
+"""
+
+variable1 = "hola"  # variable que alamcena una cadena de caracteres 
+variable2 = '619'  # variable que almacena una cadena de caracteres 
+print (variable1 + variable2)  # concatena 2 cadenas 
