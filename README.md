aeeb26769bf2d79e7656688557d945a1d09fd2a3
dany <122809726+Daniela-meneses@users.noreply.github.com>
2023-01-24 19:58:23 +0000

    creamos programa2.py

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 |--|--|--|
 |1.|Programa0.py|Hola mundo en python|
 |2.|Programa1.py|programa para ver programas multilinea,comentarios de una linea,concatenar cadenas|
+|3.|Programa2.py| concatenacion usando forma|
---
+++ semana2/programa2.py
@@ -0,0 +1,8 @@
+"""programa2
+Nombre: Daniela M,V
+Fecha: 24/01/23
+Descripcion: concatenacion usando forma
+"""
+variable1 = "hola"  # variable tipo str
+variable2 = 619  # variable tipo int
+print ("{} {}" .format(variable1 , variable2))  # concatena 2 cadenas 
