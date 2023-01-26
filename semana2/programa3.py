af7ffc8264fe5dcfa3461b56d6166a47e25d411a
dany <122809726+Daniela-meneses@users.noreply.github.com>
2023-01-26 19:49:49 +0000

    acceso a las variables por posición y por nombre

--- semana2/programa3.py
+++ semana2/programa3.py
@@ -1,14 +1,16 @@
-"""programa2
+"""programa3
 Nombre: Daniela M,V
 Fecha: 24/01/23
-Descripcion: utilizaremosoperaciones aritmeticas 
+Descripcion: utilizaremos operaciones aritmeticas 
 """
 numero1 = 10
 numero2 = 5 
-print("es el resultado de sumar {} + {} ".format(numero1 , numero2))
+print("es el resultado de sumar {} + {}".format(numero1 , numero2))
 print(numero1 + numero2)
 print("el resultado de multiplicar {} * {} = ".format(numero2 , numero1))
 print(numero1 * numero2)
-print("el resultado de dividir {} / {} = ".format(numero1 , numero2))
-print(numero1 / numero2)
+numero3 = 12
+numero4 = 3
+print("el resultado de dividir {} / {} = ".format(numero3 , numero4))
+print(numero3 / numero4)
 
---
+++ semana2/programa4.py
@@ -0,0 +1,13 @@
+"""programa4
+Nombre: Daniela M,V
+Fecha: 26/01/23
+Descripcion: acceso a las variables por posicion y por nombre 
+"""
+
+numero1 = 10
+numero2 = 5
+print("{} + {} = {}".format (numero1 , numero2 , numero1 + numero2)) 
+print("{n1} + {n2} = {suma}".format(n1=numero1 , n2=numero2 , suma=numero1 + numero2))
+print("{n2} + {n2} = {n2}".format(n1=numero1 , n2=numero2 , suma=numero1 + numero2))
+print("{numero1} + {numero2} = {suma}".format(numero1=numero1 , numero2=numero2 , suma=numero1 + numero2))
+print("{numero1} + {numero2} = {}".format (numero1 , numero2 , numero1 + numero2)) 
