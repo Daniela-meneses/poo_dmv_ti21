02678711b3315cb1388c89eee7597d2ee8b60322
dany <122809726+Daniela-meneses@users.noreply.github.com>
2023-01-30 16:56:03 +0000

    creamos el archivo de operaciones básicas

---
+++ semana3/programa5.py
@@ -0,0 +1,17 @@
+"""programa5
+Nombre: Daniela M,V
+Fecha: 30/01/23
+Descripcion: operaciones basicas
+"""
+numero1 = input("Número1 :")
+numero2 = input("Número2 :")
+suma = int(numero1) + int(numero2)
+print(suma)
+resta = int(numero1) - int(numero2)
+print(resta)
+multiplicacion = int(numero1) * int(numero2)
+print(multiplicacion)
+division = int(numero1) / int(numero2)
+print(division)
+potencia = int(numero1) ** int(numero2)
+print(potencia)
